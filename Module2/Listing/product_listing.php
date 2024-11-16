567adba6ed7711eca116792ae04d18eb3e3dba5d
nicole-njeri <nicole.njeri@strathmore.edu>
2024-11-16 14:25:06 +0300

    currency change

--- Module2/Listing/product_listing.php
+++ Module2/Listing/product_listing.php
@@ -72,7 +72,7 @@
                 <img src="uploads/<?php echo $product['ProductImage']; ?>" alt="<?php echo $product['ProductName']; ?>" class="card-img-top">
                 <div class="card-body">
                     <h5 class="card-title"><?php echo $product['ProductName']; ?></h5>
-                    <h4><?php echo "$" . $product['Price']; ?></h4>
+                    <h4><?php echo "Ksh" . $product['Price']; ?></h4>
                     <form method="post" action="../../Module3/add_to_cart.php">
                         <input type="hidden" name="product_id" value="<?php echo $product['ProductID']?>">
                         <button type="submit" class="btn btn-success btn-block">Add to Cart</button>
