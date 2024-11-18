480eed8a422ecb91a5010c232ce09a5e2f31815a
huberttim <166705401+huberttim@users.noreply.github.com>
2024-11-18 15:16:26 +0300

    e

--- tryingstuff/orders.php
+++ tryingstuff/orders.php
@@ -30,7 +30,7 @@ public function viewOrders(){
             echo '<td>' . htmlspecialchars($list['Quantity']) . '</td>';
             echo '<td>' . htmlspecialchars($list['OrderDate']) . '</td>';
             echo '<td>' . htmlspecialchars($list['Address']) . '</td>';
-            echo '<td><a href="C:/xampp/htdocs/TimBuys/Module 4/Tracking_page.php?OrderID=' .htmlspecialchars($list['OrderID']).'"12 class="btn">Track</a></td>';
+            echo '<td><a href="...Module 4/Tracking_page.php?OrderID=' .htmlspecialchars($list['OrderID']).'"12 class="btn">Track</a></td>';
             // echo '<td>';
             // echo '<form method="POST" action="remove_from_cart.php" onsubmit="return confirm(\'Are you sure you want to remove this item?\');">';
             // echo '<input type="hidden" name="product_id" value="' . htmlspecialchars($item['ProductID']) . '">';
