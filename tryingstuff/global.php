97e0fc8bd72fb1676fe949fa9cc3451adb39222e
fmarsaa <Fatuma.Marsa@strathmore.edu>
2024-11-18 21:57:32 +0300

    t

--- tryingstuff/global.php
+++ tryingstuff/global.php
@@ -1,12 +1,5 @@
 <?php
-<<<<<<< HEAD
+
 $CustomerID = 1;
-=======
-<<<<<<< HEAD
-$CustomerID = 2;
-=======
-$CustomerID = 12;
->>>>>>> 9acd01bb7354d8c93e91a95ac02c9afb1497d44f
->>>>>>> 6966aec0368e6223c72f5c20fa9d9a029ec71648
 
 ?>
